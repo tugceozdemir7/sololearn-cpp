06a75f1925a1e48ba1b46c27a4ba5f44f2c2f594
tugceozdemir7 <tugceozdemir70121@gmail.com>
2021-11-14 17:08:01 +0300

    sizeofarrays

---
+++ array1/array1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+int arr[10]={};
+arr[0]=100;
+arr[9]=1000;
+cout<<arr[0]<<endl;
+cout<<arr[1]<<endl;
+cout<<arr[2]<<endl;
+cout<<arr[3]<<endl;
+cout<<arr[4]<<endl;
+cout<<arr[5]<<endl;
+cout<<arr[6]<<endl;
+cout<<arr[7]<<endl;
+cout<<arr[8]<<endl;
+cout<<arr[9]<<endl;
+
+
+
+
+
+cout<<(arr, arr + sizeof arr / sizeof arr[0]);
+    return 0;
+
+}
